ed5e11e9101b9c51702140bf910ccbf4569cda91
Keisukelin <keisukelin@gmail.com>
2020-04-20 14:00:12 +0800

    remove duplicates

--- ESP8266_DHT11_SNMP_Agent/ESP8266_DHT11_SNMP_Agent.ino
+++ ESP8266_DHT11_SNMP_Agent/ESP8266_DHT11_SNMP_Agent.ino
@@ -13,6 +13,9 @@ const char* password = "PASSWORD";
 int pinDHT11 = 5;  //GPIO5(D1)
 SimpleDHT11 dht11;
 
+int ledPin = LED_BUILTIN;
+WiFiServer server(80);
+
 WiFiUDP udp;
 SNMPAgent snmp = SNMPAgent("public");  // Starts an SMMPAgent instance with the community string 'public'
 
@@ -36,6 +39,16 @@ void setup(){
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+
+    // Start the server
+    server.begin();
+    Serial.println("Server started");
+ 
+    // Print the IP address
+    Serial.print("Use this URL to connect: ");
+    Serial.print("http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("/");
     
     // give snmp a pointer to the UDP object
     snmp.setUDP(&udp);
@@ -62,23 +75,59 @@ void loop(){
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
 
- if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-          delay(500);
-          Serial.print("Read DHT11 failed, err="); Serial.println(err);
-          return;
-        }
+  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+      delay(500);
+      Serial.print("Read DHT11 failed, err="); Serial.println(err);
+      return;
+   }
         
-        settableNumber1 = (int)temperature;
-        settableNumber2 = (int)humidity;
-        Serial.print("Humidity = ");   
-        Serial.print((int)humidity);   
-        Serial.print("% , ");   
-        Serial.print("temperature = ");   
-        Serial.print((int)temperature);   
-        Serial.println("C "); 
-        delay(1000);
+   settableNumber1 = (int)temperature;
+   settableNumber2 = (int)humidity;
+   Serial.print("Humidity = ");   
+   Serial.print((int)humidity);   
+   Serial.print("% , ");   
+   Serial.print("temperature = ");   
+   Serial.print((int)temperature);   
+   Serial.println("C ");
+   
+   // Check if a client has connected
+   WiFiClient client = server.available();
+   if (!client) {
+     delay(1000);
+     return;
+   }else{
+      // Wait until the client sends some data
+      Serial.println("new client");
+       while(!client.available()){
+        delay(1);
+      }
+ 
+      // Read the first line of the request
+      String request = client.readStringUntil('\r');
+      Serial.println(request);
+      client.flush();
+ 
+      // Return the response
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html");
+      client.println(""); //  do not forget this one
+      client.println("<!DOCTYPE HTML>");
+      client.println("<html>");
+ 
+      client.print("Humidity = ");   
+      client.print((int)humidity);   
+      client.print("% , ");   
+      client.print("temperature = ");   
+      client.print((int)temperature);   
+      client.println("C ");
+      client.println("</html>");  
+      delay(1);
+      Serial.println("Client disonnected");
+      Serial.println("");
+      delay(1000);
+   }
   
-  snmp.loop(); // must be called as often as possible
+   snmp.loop(); // must be called as often as possible
     if(snmp.setOccurred){
           
         if (settableNumber1!=0) {
